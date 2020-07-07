2cb29c319f2e3913c6332e36260478c99d038d64
Sergey K <89184083452.mail.ru>
2020-07-07 21:18:56 +0300

    Money2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
-
+    public static void main(String[] args) {
+     int balans = 2000000000;
+     int tranfer =500_000_000;
+     int total = balans + tranfer;
+     System.out.println("Итого баланс = "+ total);
+    }
 }
